05961f72382dae6e878b611b2f85586733c4b60d
Xavier Mignot <code@xmi.fr>
2023-05-16 19:27:15 -0400

    feat: Put bucket in a module

--- infra/main.tf
+++ infra/main.tf
@@ -1,11 +1,13 @@
 resource "random_pet" "pet" {}
 
-resource "aws_s3_bucket" "web" {
-  bucket = "s3-aws-tf-lab-${random_pet.pet.id}"
+module "s3_bucket" {
+  source = "./s3_bucket"
+
+  bucket_name = "s3-aws-tf-lab-${random_pet.pet.id}"
 }
 
 resource "aws_s3_object" "index" {
-  bucket       = aws_s3_bucket.web.id
+  bucket       = module.s3_bucket.bucket_id
   key          = "index.html"
   source       = "../src/index.html"
   content_type = "text/html"
---
+++ infra/s3_bucket/main.tf
@@ -0,0 +1,3 @@
+resource "aws_s3_bucket" "web" {
+  bucket = var.bucket_name
+}
