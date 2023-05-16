f430018324ee1d3850e6b6dcd6fba16ad80ba7e9
Xavier Mignot <code@xmi.fr>
2023-05-16 19:14:44 -0400

    feat: Add minimal files

---
+++ infra/versions.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region              = "ca-central-1"
+  shared_config_files = ["~/.aws/credentials"]
+}
