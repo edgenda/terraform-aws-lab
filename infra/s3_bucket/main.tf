dffa69b96454f7cccd29a42fe47682e1e99bd712
Xavier Mignot <code@xmi.fr>
2023-05-16 19:28:57 -0400

    feat: Add versioning to mitigate violations

--- infra/s3_bucket/main.tf
+++ infra/s3_bucket/main.tf
@@ -1,3 +1,11 @@
 resource "aws_s3_bucket" "web" {
   bucket = var.bucket_name
 }
+
+resource "aws_s3_bucket_versioning" "web" {
+  bucket = aws_s3_bucket.web.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
